9793483774d02e17b4aafcfa10f2adf4f1666c3c
ZEF2DEF <kpreyer@metlife.com>
2017-10-23 16:33:53 -0600

    commit to move NgApp to _Layout.cshtml

--- MLRSingleChannel/Controllers/HomeController.cs
+++ MLRSingleChannel/Controllers/HomeController.cs
@@ -9,13 +9,23 @@ namespace MLRSingleChannel.Controllers
   [RoutePrefix("")]
   public class HomeController : Controller
   {
-
-    [Route("")]
     public ActionResult Index()
     {
-      ViewBag.Title = "Home Page";
+      return View();
+    }
 
-      return View("NgApp");
+    public ActionResult About()
+    {
+      ViewBag.Message = "Your application description page.";
+
+      return View();
+    }
+
+    public ActionResult Contact()
+    {
+      ViewBag.Message = "Your contact page.";
+
+      return View();
     }
   }
 }
